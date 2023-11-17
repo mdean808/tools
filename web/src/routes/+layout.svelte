bb965913f1cd76bdc09596ae4c0f7434649655a9
Morgan Dean <morgandeanhi@gmail.com>
2023-11-17 13:59:30 -0800

    add google analytics

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,5 +1,26 @@
 <script>
 	import '../app.postcss';
+	import { initializeApp } from 'firebase/app';
+	import { getAnalytics } from 'firebase/analytics';
+	import { onMount } from 'svelte';
+
+	// Your web app's Firebase configuration
+	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+	const firebaseConfig = {
+		apiKey: 'AIzaSyAK8TjX2xfHznSKurF8v-vJkc4m3qvLX1M',
+		authDomain: 'morgan-s-tools.firebaseapp.com',
+		projectId: 'morgan-s-tools',
+		storageBucket: 'morgan-s-tools.appspot.com',
+		messagingSenderId: '229257882277',
+		appId: '1:229257882277:web:2f63caa27ebd248849fb4c',
+		measurementId: 'G-3HTHYLK8NN'
+	};
+
+	// Initialize Firebase
+	onMount(() => {
+		const app = initializeApp(firebaseConfig);
+		const analytics = getAnalytics(app);
+	});
 </script>
 
 <svelte:head>
